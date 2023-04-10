dfd543ed6d6b725ce345ac9dce986d1e5bb6b991
Gabriel Aguero <gabrielhaguero@gmail.com>
2023-04-10 10:24:58 -0300

    cargando archivos

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,15 @@ import Nosotros from './componentes/paginas/Nosotros.jsx';
 import Contacto from './componentes/paginas/Contacto.jsx';
 import ProductList from './componentes/paginas/ProductList';
 import { AuthProvider } from './context/AuthContext.jsx';
-import CarouselComponents from './componentes/Layout/Carousel';
-import Footer from './componentes/Layout/Footer';
 import Main from './componentes/Layout/Main';
-import ComoComprar from './componentes/paginas/ComoComprar';
-import Visitanos from './componentes/Layout/Visitanos';
+
 
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <NavBarComponents />
-        <CarouselComponents />
+        <NavBarComponents />      
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="categoria/:categoriaName" element={ <ProductList /> }/>          
@@ -28,9 +24,6 @@ function App() {
           <Route path="contacto" element={<Contacto />} />
           <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
-        <ComoComprar />
-        <Visitanos />        
-      <Footer />
       </BrowserRouter>
     </AuthProvider>
   );
