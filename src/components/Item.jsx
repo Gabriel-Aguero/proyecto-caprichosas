ba927615c1d305c3effde5f60bd364232e248d64
Gabriel Aguero <gabrielhaguero@gmail.com>
2023-04-27 13:02:00 -0300

    cargando banner

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,10 +5,10 @@ import { Card } from 'react-bootstrap';
 const Item = ({item}) => {
     return (
         <Link to={"/item/" + item.id} className="text-decoration-none">
-            <Card className="text-center text-md-start">
+            <Card className="text-center text-md-center">
                 <Card.Img variant="top" src={item.imagen} style={{height: "200px", objectFit: 'cover'}}/>
                 <Card.Body>
-                    <Card.Title className='font fw-medium' style={{ marginTop: '2px' }}>{item.nombre}</Card.Title>
+                    <Card.Title className='font-bold fw-medium' style={{ marginTop: '2px' }}>{item.nombre}</Card.Title>
                     
                         <Card.Text>
                             {"$" + item.precio + " ARS"}
